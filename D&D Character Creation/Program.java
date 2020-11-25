2e3f0d9c682cd35ae22bfc0769607ed16d45940a
Justin Allen Creighton <46484674+jcreighton669@users.noreply.github.com>
2020-11-25 09:57:35 -0500

    Update Program.java

--- D&D Character Creation/Program.java
+++ D&D Character Creation/Program.java
@@ -1,8 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2020 Justin Allen Creighton
  */
+
 package program;
 import java.util.Random;
 import java.io.*;
@@ -258,7 +257,7 @@ public class Program {
         int firstNumber = RPGCharacter.rollDice(12);
         int secondNumber = RPGCharacter.rollDice(9);
         int thirdNumber = RPGCharacter.rollDice(9);
-        System.out.println("Enter your name: ");
+        System.out.println("Enter your characters name (optional): ");
         String name = Keyboard.readLine();
         myChar.name = name;
         System.out.println("=====================================");
